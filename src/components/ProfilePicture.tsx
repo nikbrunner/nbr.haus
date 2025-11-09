c07d5c74312bf43b07d5dbbc6266a644b491b2a1
nbr <nikolaus.brunner@protonmail.ch>
2025-11-09 14:04:59 +0100

    refactor: extract content blocks into separate components

--- src/components/ProfilePicture.tsx
+++
@@ -1,8 +0,0 @@
-<div class="profile-picture">
-  <img
-    class="profile-image"
-    src={profile_picture.src}
-    alt="Nik Brunner's personal website - a black and white photograph of a modernist house representing his digital home"
-  />
-  <div class="profile-image-overlay"></div>
-</div>;
