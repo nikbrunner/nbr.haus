fe8273d6fd2301d0f48a6f1f8c91a4370cd63052
nbr <nikolaus.brunner@protonmail.ch>
2025-11-15 11:45:36 +0100

    chore: remove Astro backup folder

--- _astro-backup/src/components/AccentPicker.module.css.d.ts
+++
@@ -1,7 +0,0 @@
-declare const styles: {
-  readonly "accentPicker": string;
-  readonly "circle": string;
-  readonly "spin": string;
-};
-export = styles;
-
--- _astro-backup/src/components/Job.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "company": string;
-  readonly "job": string;
-  readonly "period": string;
-  readonly "role": string;
-  readonly "roleAndPeriod": string;
-};
-export = styles;
-
