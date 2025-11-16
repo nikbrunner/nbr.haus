be511e251ebcb8c1fa3f84f76171c3c756ff36ec
nbr <nikolaus.brunner@protonmail.ch>
2025-11-16 14:42:44 +0100

    refactor(content): update About

--- src/content-blocks/index/About.tsx
+++ src/content-blocks/index/About.tsx
@@ -1,4 +1,5 @@
-import Highlight from "../../components/Highlight";
+import Highlight from "@/components/Highlight";
+import Hr from "@/components/Hr";
 
 export default function About() {
   const experienceInYears = new Date().getFullYear() - 2020;
@@ -8,9 +9,23 @@ export default function About() {
       <p>
         I'm <Highlight>Nik Brunner</Highlight>, a Software Engineer with{" "}
         {experienceInYears} years of experience building frontend architectures
-        and design systems. Currently seeking new opportunities starting
-        February 2026.
+        and design systems.
       </p>
+
+      <p>
+        I work closely with designers and have a strong sense for UX (user
+        experience) and DX (developer experience). I'm also comfortable working
+        independently and making design decisions when needed.
+      </p>
+
+      <p>
+        <Highlight>
+          Currently seeking new opportunities starting February 2026.
+        </Highlight>
+      </p>
+
+      <Hr />
+
       <p>
         Born in 1984, based in Landshut, Germany. Outside of code, I enjoy
         hiking, running, and photography.
