829e596b3b286360a1d93e78c2e3c2878f238a21
nbr <nikolaus.brunner@protonmail.ch>
2025-12-14 21:04:15 +0100

    style(StylePicker): account for safe area insets on mobile

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,7 +33,7 @@ export const Route = createRootRoute({
       },
       {
         name: "viewport",
-        content: "width=device-width"
+        content: "width=device-width, initial-scale=1, viewport-fit=cover"
       },
       {
         name: "description",
